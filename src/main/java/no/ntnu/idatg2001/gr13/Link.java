2236c953c974223abcf1d6a607aff898aa1a7dcd
arthurThorkildsen <arthurbt@stud.ntnu.no>
2023-02-06 14:19:37 +0100

    creating constructor and fields for Link class

--- src/main/java/no/ntnu/idatg2001/gr13/Link.java
+++ src/main/java/no/ntnu/idatg2001/gr13/Link.java
@@ -1,2 +1,17 @@
-package no.ntnu.idatg2001.gr13;public class Link {
+package no.ntnu.idatg2001.gr13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Link
+{
+    private final String text;
+    private String reference;
+    private List<String> action;
+
+    public Link(String text, String reference, List action){
+        this.text = text;
+        this.reference = reference;
+        this.action = action;
+    }
 }
