50a043db66081f475fcd1728547168420063379d
arthurThorkildsen <arthurbt@stud.ntnu.no>
2023-02-06 14:49:35 +0100

    "#1 fixing up link and main"

--- src/main/java/no/ntnu/idatg2001/gr13/Link.java
+++ src/main/java/no/ntnu/idatg2001/gr13/Link.java
@@ -2,16 +2,36 @@ package no.ntnu.idatg2001.gr13;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.Action;
 
 public class Link
 {
-    private final String text;
+    private String text;
     private String reference;
-    private List<String> action;
+    private List<Action> action;
 
-    public Link(String text, String reference, List action){
+    public Link(String text, String reference){
+        setReference(reference);
+        setText(text);
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
         this.text = text;
+    }
+
+    public String getReference()
+    {
+        return reference;
+    }
+
+    public void setReference(String reference)
+    {
         this.reference = reference;
-        this.action = action;
     }
 }
--- src/main/java/no/ntnu/idatg2001/gr13/Main.java
+++ src/main/java/no/ntnu/idatg2001/gr13/Main.java
@@ -1,9 +1,13 @@
 package no.ntnu.idatg2001.gr13;
 
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("blue");
+        Link link = new Link("hello", "bad news");
     }
 }
