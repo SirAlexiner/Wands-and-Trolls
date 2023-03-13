3e8b49afa6c285d12f06e79ac93989b1f81bf4d3
arthurThorkildsen <arthurbt@stud.ntnu.no>
2023-03-13 17:21:48 +0100

    #6 1.iteration of writeToFile method

--- src/main/java/no/ntnu/idatg2001/gr13/StoryFileHandler.java
+++ src/main/java/no/ntnu/idatg2001/gr13/StoryFileHandler.java
@@ -1,6 +1,9 @@
 package no.ntnu.idatg2001.gr13;
 
+import static java.nio.file.Files.newBufferedWriter;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,8 +11,23 @@ import java.util.ArrayList;
 
 public class StoryFileHandler
 {
-    public static ArrayList<Story> readCSV(String fileName){
-        ArrayList<Story> stories = new ArrayList<>();
+    public static void writeToFile(Story story, String fileName){
+        // Try-with-resource-statement"
+        try(BufferedWriter writer = newBufferedWriter(Path.of(fileName))){
+            writer.write(story.getTitle() + "\n\n");
+            for (Passage passage: story.getPassages())
+            {
+                writer.write("::" + passage.getReference() + "\n");
+                writer.write(passage.getContent() + "\n");
+                writer.write("[" + passage.getLink(passage.getContent()) + "]" + "(" + ")");
+            }
+        }
+        catch (IOException e){
+            System.err.println("There was a problem writing to" + fileName);
+        }
+
+    }
+    public static Story readToFile(String fileName){
         try(BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
             String lineOfText;
             while ((lineOfText = reader.readLine()) != null){
@@ -20,6 +38,7 @@ public class StoryFileHandler
         catch (IOException e){
 
         }
-        return stories;
+        Story Story = null;
+        return Story;
     }
 }
