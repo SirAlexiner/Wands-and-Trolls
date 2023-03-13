3a0023fb485a418dd63c87cc07d69bef64a047e0
arthurThorkildsen <arthurbt@stud.ntnu.no>
2023-03-13 13:59:39 +0100

    updated gitIgnore, and added a simple readCSV method

--- src/main/java/no/ntnu/idatg2001/gr13/StoryFileHandler.java
+++ src/main/java/no/ntnu/idatg2001/gr13/StoryFileHandler.java
@@ -1,8 +1,25 @@
 package no.ntnu.idatg2001.gr13;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class StoryFileHandler
 {
+    public static ArrayList<Story> readCSV(String fileName){
+        ArrayList<Story> stories = new ArrayList<>();
+        try(BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
+            String lineOfText;
+            while ((lineOfText = reader.readLine()) != null){
+                String[] words = lineOfText.split("");
+            }
 
+        }
+        catch (IOException e){
+
+        }
+        return stories;
+    }
 }
