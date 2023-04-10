b8df5b7a61380ea6558459e7b969d7ab3793da62
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-04-11 00:05:25 +0200

    Added InventoryActiontest class with test methods

---
+++ src/test/java/InventoryActionTest.java
@@ -0,0 +1,45 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import no.ntnu.idatg2001.gr13.Player;
+import no.ntnu.idatg2001.gr13.actions.InventoryAction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+class InventoryActionTest {
+    String item = "Item example";
+    Player player;
+    InventoryAction inventoryAction;
+    @BeforeEach
+    void setUp(){
+        player = new Player("Test Player", 10, 10, 10);
+        inventoryAction = new InventoryAction(item);
+    }
+
+    @Test
+    void testCanExecute() {
+        boolean actual = inventoryAction.canExecute(player);
+        assertFalse(actual);
+    }
+
+    @Test
+    void testGetActionType() {
+        String expected = "Inventory";
+        String actual = inventoryAction.getActionType();
+        // Positive
+        assertEquals(expected, actual);
+        // Negative
+        String unexpected = "Gold";
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    void testGetActionValue() {
+        String expected = item;
+        String actual = inventoryAction.getActionValue();
+        // Positive
+        assertEquals(expected, actual);
+        // Negative
+        String unexpected = "Gold";
+        assertNotEquals(unexpected, actual);
+    }
+}
+
