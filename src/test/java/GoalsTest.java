154b0639fc3368fac180f083fc91fdc1b9aa540d
arthurThorkildsen <arthurbt@stud.ntnu.no>
2023-04-12 18:52:14 +0200

    Finished test for goldGoals class

---
+++ src/test/java/GoalsTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import no.ntnu.idatg2001.gr13.Player;
+import no.ntnu.idatg2001.gr13.goals.GoldGoals;
+import no.ntnu.idatg2001.gr13.goals.HealthGoal;
+import no.ntnu.idatg2001.gr13.goals.InventoryGoal;
+import no.ntnu.idatg2001.gr13.goals.ScoreGoal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class GoalsTest
+{
+    int gold = 10;
+    int health = 10;
+    int score = 10;
+    String sword = "Sword";
+    String shield = "Shield";
+    List<String> expectedInventory;
+    Player player;
+    GoldGoals goldGoals;
+    HealthGoal healthGoal;
+    ScoreGoal scoreGoal;
+    InventoryGoal inventoryGoal;
+    boolean conditionToBeChecked;
+    @BeforeEach
+    void setUp(){
+        player = new Player("Test Player", health, gold, score);
+        expectedInventory = new ArrayList<>();
+    }
+
+    @Test
+    void testIsFulFilledGoldGoals(){
+        goldGoals = new GoldGoals(gold);
+        conditionToBeChecked = goldGoals.isFulfilled(player);
+        assertTrue(conditionToBeChecked);
+
+        player.addGold(-1);
+        conditionToBeChecked = goldGoals.isFulfilled(player);
+        assertFalse(conditionToBeChecked);
+    }
+}
