779e5259b1c2034751adda874ed243cb313afbef
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-03 11:08:30 +0200

    updated pom for java fx and dark theme

--- src/main/java/io/github/mimoguz/customwindow/StageOps.java
+++ src/main/java/io/github/mimoguz/customwindow/StageOps.java
@@ -1,6 +1,5 @@
 package io.github.mimoguz.customwindow;
 
-import com.sun.jna.*;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinError;
@@ -117,3 +116,4 @@ public class StageOps {
         | floatingTo8Bit(color.getRed());
   }
 }
+/*
