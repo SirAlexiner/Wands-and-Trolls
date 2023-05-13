7022e0049a1bffcc30125e53e8ef0f9be5764746
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-13 18:54:40 +0200

    Updated main import statement to remove errors

--- src/main/java/no/ntnu/idatg2001/gr13/Main.java
+++ src/main/java/no/ntnu/idatg2001/gr13/Main.java
@@ -1,6 +1,6 @@
 package no.ntnu.idatg2001.gr13;
 
-import no.ntnu.idatg2001.gr13.view.MainMenuView;
+import no.ntnu.idatg2001.gr13.view.*;
 
 public class Main {
     public static void main(String[] args) {
