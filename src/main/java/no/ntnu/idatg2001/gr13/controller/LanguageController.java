b853fe06cd0da4cbe4efd6b95f9cc5f13ea32e42
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-14 22:50:11 +0200

    Refactored, moved and renamed to LanguageController

--- src/main/java/no/ntnu/idatg2001/gr13/controller/LanguageController.java
+++ src/main/java/no/ntnu/idatg2001/gr13/controller/LanguageController.java
@@ -1,22 +1,30 @@
-package no.ntnu.idatg2001.gr13.model;
+package no.ntnu.idatg2001.gr13.controller;
+
+import no.ntnu.idatg2001.gr13.view.LanguageListener;
+import no.ntnu.idatg2001.gr13.model.ResourceBundles;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class LanguageModel {
+import static no.ntnu.idatg2001.gr13.model.ResourceBundles.BUTTONS;
+
+public class LanguageController {
     private ResourceBundle bundle;
     private List<LanguageListener> listeners;
     // TODO singleton
 
-    public LanguageModel() {
+    public LanguageController() {
         listeners = new ArrayList<>();
     }
 
     public void setLanguage(String languageCode) {
-        Locale locale = new Locale.Builder().setLanguage(languageCode).build();
-        bundle = ResourceBundle.getBundle("languages/buttons", locale);
+        setLanguageForObject(BUTTONS, new Locale.Builder().setLanguage(languageCode).build());
+    }
+
+    public void setLanguageForObject(ResourceBundles resourceObject, Locale locale) {
+        bundle = ResourceBundle.getBundle("languages/" + resourceObject.getBundleName(), locale);
         notifyLanguageChange();
     }
 
@@ -26,10 +34,6 @@ public class LanguageModel {
 
     public void addLanguageChangeListener(LanguageListener listener) {
         listeners.add(listener);
-    }
-
-    public void removeLanguageChangeListener(LanguageListener listener) {
-        listeners.remove(listener);
     }
 
     public void notifyLanguageChange() {
