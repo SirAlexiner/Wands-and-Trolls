c1b0e7a8278c9846dd1f053fb56623372774642e
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-14 21:31:02 +0200

    Updated module-info.java to resolve errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,22 +5,15 @@
  * @since May 14. 2023
  */
 module PathsModule {
-
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires com.sun.jna;
     requires com.sun.jna.platform;
-    requires atlantafx.base;
-    requires com.jthemedetector;
-    requires java.xml;
-    requires org.kordamp.ikonli.feather;
-    requires javafx.fxml;
-    requires java.desktop;
+    requires javafx.graphics;
     requires lombok;
-    requires org.jetbrains.annotations;
-    requires java.logging;
+    requires com.sun.jna;
+    requires javafx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires atlantafx.base;
+    requires org.kordamp.ikonli.feather;
+    requires com.jthemedetector;
 
-    exports no.ntnu.idatg2001.gr13.controller;
     exports no.ntnu.idatg2001.gr13.view;
-    exports no.ntnu.idatg2001.gr13.model;
 }
