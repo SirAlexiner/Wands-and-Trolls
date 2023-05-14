d6e4622f731b5bd6d90acbcf5957c640775638fc
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-14 19:19:27 +0200

    Updated LanguageModel to correctly implement listeners

--- src/main/java/no/ntnu/idatg2001/gr13/model/LanguageModel.java
+++ src/main/java/no/ntnu/idatg2001/gr13/model/LanguageModel.java
@@ -1,24 +1,41 @@
 package no.ntnu.idatg2001.gr13.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LanguageModel {
     private ResourceBundle bundle;
+    private List<LanguageListener> listeners;
     // TODO singleton
+
+    public LanguageModel() {
+        listeners = new ArrayList<>();
+    }
 
     public void setLanguage(String languageCode) {
         Locale locale = new Locale.Builder().setLanguage(languageCode).build();
         bundle = ResourceBundle.getBundle("languages/buttons", locale);
-        // Notify the views about the language change
+        notifyLanguageChange();
     }
 
     public String getLocalizedString(String key) {
         return bundle.getString(key);
     }
 
-    public void addLanguageChangeListener(LanguageChangeListener listener) {
+    public void addLanguageChangeListener(LanguageListener listener) {
         listeners.add(listener);
+    }
+
+    public void removeLanguageChangeListener(LanguageListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void notifyLanguageChange() {
+        for (LanguageListener listener : listeners) {
+            listener.languageChange();
+        }
     }
 }
 
