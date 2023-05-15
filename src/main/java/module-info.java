dad47d9e0cd2b30c6653b66c6b1aece234838581
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-15 16:49:34 +0200

    Updated pom and module info to comply with maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/**
+ * This is the module info for the WiNG application.
+ *
+ * @author Arthur Borger Thorkildsen
+ * @since May 14. 2023
+ */
+module PathsModule {
+    requires com.sun.jna.platform;
+    requires javafx.graphics;
+    requires lombok;
+    requires com.sun.jna;
+    requires javafx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires atlantafx.base;
+    requires org.kordamp.ikonli.feather;
+    requires com.jthemedetector;
+
+    exports no.ntnu.idatg2001.gr13.view;
+    exports no.ntnu.idatg2001.gr13.model;
+}
