74a709f2032f31679bdd43e477d6e7a93f58fa33
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-15 21:05:18 +0200

    Updated Game class

--- src/main/java/no/ntnu/idatg2001/gr13/model/Game.java
+++ src/main/java/no/ntnu/idatg2001/gr13/model/Game.java
@@ -1,63 +1,56 @@
 package no.ntnu.idatg2001.gr13.model;
 
 import java.util.List;
+
 import lombok.Getter;
 import no.ntnu.idatg2001.gr13.model.goals.Goal;
 
-public record Game(@Getter Player player, @Getter Story story, @Getter List<Goal> goals) {
+/**
+ * A class representing a game, part of the WiNG application, package model.
+ */
+public class Game {
 
+  @Getter
+  private final Player player;
+  @Getter
+  private final Story story;
+  @Getter
+  private final List<Goal> goals;
+
+  /**
+   * The Game's constructor. Throws an exception if the player, story or goals are empty.
+   *
+   * @param player The player object in the game.
+   * @param story  The story to be played in the game.
+   * @param goals  The list of goal objects.
+   * @throws IllegalArgumentException
+   */
+  public Game(Player player, Story story, List<Goal> goals) throws IllegalArgumentException {
+    if (player != null || story != null || !goals.isEmpty()) {
+      this.player = player;
+      this.story = story;
+      this.goals = goals;
+    } else {
+      throw new IllegalArgumentException("Game values not initialized. Values cannot be null");
+    }
+  }
+
+  /**
+   * A method for returning the opening passage of the story.
+   *
+   * @return The opening passage of the story.
+   */
   public Passage begin() {
     return story.getOpeningPassage();
   }
 
+  /**
+   * A method for returning a passage in the story that the link points to.
+   *
+   * @param link The link that was clicked.
+   * @return The passage value to the link key.
+   */
   public Passage go(Link link) {
     return story.getPassage(link);
-  }
-
-  public static Game setup() {
-    /*
-    Player player = new Player("Adventurer", 100, 0, 0);
-    Passage startingRoom = new Passage("Starting Room", "<: A seemingly empty cave opening :>");
-    startingRoom.addLink(new Link("Enter Cave", "empty LINK reference"));
-    startingRoom.addLink(new Link( "Exit Cave", "empty LINK reference"));
-    Story story = new Story("Exit Cave", startingRoom);
-    Passage caveEntrance = new Passage("Enter Cave", "<: You've entered the troll cave," +
-        " it's very dark, but you see openings to the left and right :>");
-    Passage justLeft = new Passage("Exit Cave", "You left the cave");
-    Passage trollRoom =
-        new Passage("Go Left", "<: You've entered the room to the left and run into the troll :>");
-    Link magicSpell = new Link("Cast Magic Spell", "empty LINK reference");
-    magicSpell.addAction(new InventoryAction("Spell Scroll"));
-    trollRoom.addLink(magicSpell);
-    Passage spellRoom = new Passage("Go Right", "<: You've entered a seemingly empty room," +
-        " it's very dark! :>");
-    caveEntrance.addLink(new Link("Go Left", "empty LINK reference"));
-    caveEntrance.addLink(new Link("Go Right", "empty LINK reference"));
-    story.addPassage(caveEntrance);
-    story.addPassage(justLeft);
-    story.addPassage(trollRoom);
-    story.addPassage(spellRoom);
-    Goal gold = new GoldGoals(50);
-    Goal health = new HealthGoal(0);
-    List<String> mandatoryItems = new ArrayList<>();
-    mandatoryItems.add("Key");
-    Goal inv = new InventoryGoal(mandatoryItems);
-    Goal score = new ScoreGoal(5000);
-
-    List<Goal> goals = new ArrayList<>();
-    goals.add(gold);
-    goals.add(health);
-    goals.add(inv);
-    goals.add(score);
-
-    return new Game(player, story, goals);
-     */
-
-
-    // Read from file '.paths'
-    StoryFileHandler.readActionFromFile(".paths");
-
-
-    return null;
   }
 }
