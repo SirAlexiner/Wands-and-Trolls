6186a027499dd7644a6762d443bc48da5790009e
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-20 13:26:55 +0200

    Fixed correct implementaiotion of adding beginning passage first in StoryFileHandler

--- src/main/java/no/ntnu/idatg2001/gr13/model/StoryFileHandler.java
+++ src/main/java/no/ntnu/idatg2001/gr13/model/StoryFileHandler.java
@@ -2,18 +2,13 @@ package no.ntnu.idatg2001.gr13.model;
 
 import static java.nio.file.Files.newBufferedWriter;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import no.ntnu.idatg2001.gr13.model.actions.Action;
-import no.ntnu.idatg2001.gr13.model.actions.GoldAction;
-import no.ntnu.idatg2001.gr13.model.actions.HealthAction;
-import no.ntnu.idatg2001.gr13.model.actions.InventoryAction;
-import no.ntnu.idatg2001.gr13.model.actions.ScoreAction;
 
 /**
  * This class is part of the "WiNG" application, this class acts as a file handler.
@@ -58,7 +53,11 @@ public class StoryFileHandler
      * @throws IOException if an input/output occurs while writing to the file
      */
     private static void writeStory(Story story, BufferedWriter writer) throws IOException {
-        for (Passage passage: story.getPassages())
+        List<Passage> passagesToWrite = new ArrayList<>();
+        passagesToWrite.add(story.getOpeningPassage());
+        passagesToWrite.addAll(story.getPassages());
+
+        for (Passage passage: passagesToWrite)
         {
             writePassages(story, passage, writer);
             for (Link link : passage.getLinks()) {
@@ -116,79 +115,6 @@ public class StoryFileHandler
         // Writes the type and value of the action
         writer.write("=" + action.getActionType() + ";" + action.getActionValue() + "\n");
     }
-    /**
-     * The method reads a file and creates a story object from the file.
-     *
-     * @param fileName The name of the file to read from.
-     * @return A story object.
-     */
-    public static Story readFromFile(String fileName){
-        String lineOfText;
-        Story story = null;
-        Passage passage = null;
-        Link link = null;
-        //TODO must fix passage content implementation
-        try(BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
-            String titleOfGame = reader.readLine();
-            while ((lineOfText = reader.readLine()) != null){
-                if (lineOfText.startsWith("::")){
-                    String[] line = lineOfText.split("::");
-                    passage = new Passage(line[1], reader.readLine());
 
-                    if (story == null) {
-                        story = new Story(titleOfGame, passage);
-                    }
-                    story.addPassage(passage);
-                }
-                if (lineOfText.startsWith("[") && (passage != null )){
-                    String[] line = lineOfText.split("\\[");
-                    String[] text = line[1].split("]");
-
-                    line = text[1].split("\\(");
-                    String[] reference = line[1].split("\\)");
-                    link = new Link(text[0], reference[0]);
-                    passage.addLink(link);
-                }
-                if (lineOfText.startsWith("=") && link != null){
-                    // Adds an action to the link object.
-                    link.addAction(readActionFromFile(lineOfText));
-                }
-            }
-        }
-        catch (IOException e){
-            System.out.println(e);
-        }
-        return story;
-    }
-
-    /**
-     * A method for reading action from a CSV file format.
-     * @param actionFromFile A string with the action to be read.
-     * @return the Action.
-     */
-    private static Action readActionFromFile(String actionFromFile){
-        String[] action = actionFromFile.split("=");
-        action = action[1].split(";");
-
-        switch (action[0]){
-            // Parsing it to integer since the input from the file is an int.
-            case ("Gold") -> {
-                return (new GoldAction(Integer.parseInt(action[1])));
-            }
-            case ("Health") -> {
-                return (new HealthAction(Integer.parseInt(action[1])));
-            }
-            case ("Score") -> {
-                return (new ScoreAction(Integer.parseInt(action[1])));
-            }
-            // Inventory action uses String as datatype.
-            case ("Inventory") -> {
-                return (new InventoryAction(action[1]));
-            }
-
-            default -> System.err.print("error in switch");
-        }
-        return null;
-    }
 
 }
