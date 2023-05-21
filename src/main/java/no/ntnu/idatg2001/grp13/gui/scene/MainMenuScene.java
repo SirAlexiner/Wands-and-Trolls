9eef1fe1fc7fcf5807a4fa7cd4a3a3e097ccf361
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-21 14:03:38 +0200

    Refactored MainMenuScene with helper methods

--- src/main/java/no/ntnu/idatg2001/grp13/gui/scene/MainMenuScene.java
+++ src/main/java/no/ntnu/idatg2001/grp13/gui/scene/MainMenuScene.java
@@ -32,6 +32,34 @@ public class MainMenuScene {
   private StackPane contentContainer;
 
   public static StackPane getScene(Stage stage) {
+    HBox logoBox = createLogoBox();
+    VBox mainButtonBox = createMainButtonBox(stage);
+    BorderPane bottomInformation = createBottomInformation();
+    Image background = new Image(
+        Objects.requireNonNull(MainStage.class.getResourceAsStream(
+            "/Image/MainMenu/Main_Menu.gif")));
+    ImageView backgroundView = new ImageView(background);
+    backgroundView.setFitWidth(1024);
+    backgroundView.setFitHeight(768);
+
+    BorderPane content = new BorderPane();
+    content.getChildren().add(backgroundView);
+    content.setTop(logoBox);
+    content.setCenter(mainButtonBox);
+    content.setBottom(bottomInformation);
+    contentContainer = new StackPane(content);
+    contentContainer.setPrefHeight(660);
+    Rectangle clipRect = createClipRect();
+    contentContainer.setClip(clipRect);
+
+    if (USE_AI) {
+      loadImage(content);
+    }
+
+    return contentContainer;
+  }
+
+  private HBox createLogoBox() {
     Image logo =
         new Image(String.valueOf(MainStage.class.getResource(
             "/Image/MainMenu/WnT_logo_caption.png")));
@@ -44,58 +72,91 @@ public class MainMenuScene {
     logoBox.setPadding(new Insets(50, 20, 20, 20));
     logoBox.getChildren().add(logoView);
 
+    return logoBox;
+  }
+
+  private VBox createMainButtonBox(Stage stage) {
     VBox box = new VBox();
     box.setSpacing(10);
     box.setAlignment(Pos.CENTER);
+    box.getChildren().addAll(
+        createNewGameButton(stage),
+        createLoadButton(stage),
+        createCreateButton(),
+        createSettingsButton(stage)
+    );
+
+    return box;
+  }
+
+  private Button createNewGameButton(Stage stage) {
     Button newGameButton = new FantasyButton("New Game");
     newGameButton.setOnMouseClicked(event -> {
       Scene newGameScene = NewGame.getNewGameScene(stage);
       contentContainer.getChildren().add(newGameScene.getRoot());
     });
     newGameButton.setPrefWidth(225);
-    box.getChildren().add(newGameButton);
+    return newGameButton;
+  }
 
-    BorderPane content = new BorderPane();
-    contentContainer = new StackPane(content);
-    contentContainer.setPrefHeight(660);
-
+  private Button createLoadButton(Stage stage) {
     Button loadButton = new FantasyButton("Load Adventure");
     loadButton.setOnMouseClicked(event -> {
       Scene filePickerScene = FilePicker.getFilePickerScene(stage);
       contentContainer.getChildren().add(filePickerScene.getRoot());
     });
     loadButton.setPrefWidth(225);
-    box.getChildren().add(loadButton);
+    return loadButton;
+  }
 
+  private Button createCreateButton() {
     Button createButton = new FantasyButton("Create Adventure");
     createButton.setPrefWidth(225);
-    box.getChildren().add(createButton);
+    return createButton;
+  }
 
+  private Button createSettingsButton(Stage stage) {
     Button settingsButton = new FantasyButton("Settings");
     settingsButton.setPrefWidth(225);
-    box.getChildren().add(settingsButton);
     settingsButton.setOnMouseClicked(event -> {
       Scene settingScene = SettingsScene.getSettingScene(stage);
       contentContainer.getChildren().add(settingScene.getRoot());
     });
+    return settingsButton;
+  }
+
+  private BorderPane createBottomInformation() {
+    HBox versionInformation = createVersionInformation();
+    HBox copyrightInformation = createCopyrightInformation();
+    HBox emptySpacer = createEmptySpacer();
 
     BorderPane bottomInformation = new BorderPane();
     bottomInformation.setPadding(new Insets(20));
+    bottomInformation.setLeft(versionInformation);
+    bottomInformation.setCenter(copyrightInformation);
+    bottomInformation.setRight(emptySpacer);
 
+    return bottomInformation;
+  }
+
+  private HBox createVersionInformation() {
     HBox versionInformation = new HBox();
-    Label version =
-        new Label("Version: " + App.getVersion() + "\nBuild: " + App.getBuild());
+    Label version = new Label("Version: " + App.getVersion() + "\nBuild: " + App.getBuild());
     versionInformation.setAlignment(Pos.BOTTOM_LEFT);
     versionInformation.getChildren().add(version);
+    return versionInformation;
+  }
 
+  private HBox createCopyrightInformation() {
+    VBox companyLogoBox = createCompanyLogoBox();
     HBox copyrightInformation = new HBox();
     copyrightInformation.setAlignment(Pos.BOTTOM_CENTER);
     copyrightInformation.setSpacing(25);
+    copyrightInformation.getChildren().add(companyLogoBox);
+    return copyrightInformation;
+  }
 
-    VBox companyLogoBox = new VBox();
-    companyLogoBox.setAlignment(Pos.CENTER);
-    companyLogoBox.setSpacing(10);
-
+  private VBox createCompanyLogoBox() {
     Image companyLogo =
         new Image(
             String.valueOf(MainStage.class.getResource(
@@ -107,39 +168,28 @@ public class MainMenuScene {
     Label copyright = new Label("Copyright © 2023 - " + LocalDate.now().getYear()
         + ": Thunder Entertainment. All Rights Reserved.");
 
+    VBox companyLogoBox = new VBox();
+    companyLogoBox.setAlignment(Pos.CENTER);
+    companyLogoBox.setSpacing(10);
     companyLogoBox.getChildren().addAll(companyLogoview, copyright);
 
-    copyrightInformation.getChildren().add(companyLogoBox);
+    return companyLogoBox;
+  }
 
-
+  private HBox createEmptySpacer() {
     HBox emptySpacer = new HBox();
     emptySpacer.setPrefWidth(150);
+    return emptySpacer;
+  }
 
-    bottomInformation.setLeft(versionInformation);
-    bottomInformation.setCenter(copyrightInformation);
-    bottomInformation.setRight(emptySpacer);
-
-    Image background = new Image(
-        Objects.requireNonNull(MainStage.class.getResourceAsStream(
-            "/Image/MainMenu/Main_Menu.gif")));
-    ImageView backgroundView = new ImageView(background);
-    backgroundView.setFitWidth(1024);
-    backgroundView.setFitHeight(768);
-
-    content.getChildren().add(backgroundView);
-
-    content.setTop(logoBox);
-    content.setCenter(box);
-    content.setBottom(bottomInformation);
-
-    // Create a rectangle with rounded top corners
+  private Rectangle createClipRect() {
     Rectangle clipRect = new Rectangle(1024, 660);
     clipRect.setArcWidth(30);
     clipRect.setArcHeight(30);
+    return clipRect;
+  }
 
-    contentContainer.setClip(clipRect);
-
-
+  private void loadImage(BorderPane content) {
     Task<Image> loadImageTask = new Task<>() {
       @Override
       protected Image call() {
@@ -148,38 +198,28 @@ public class MainMenuScene {
                 + " Realistic Digital Art,"
                 + " You finally managed to climb the mountain,"
                 + " in the distance you see a cave opening with a troll guarding the entrance.";
-        if (USE_AI) {
-          try {
-            return OpenAiImage.generateImage(description);
-          } catch (Exception e) {
-            ErrorLogger.LOGGER.log(Level.SEVERE,
-                String.format("Failed to load AI Image: %s", e));
-            return new Image(Objects.requireNonNull(
-                MainStage.class.getResourceAsStream("/Image/Window/Background.png")));
-          }
-        } else {
-          return null;
+        try {
+          return OpenAiImage.generateImage(description);
+        } catch (Exception e) {
+          ErrorLogger.LOGGER.log(Level.SEVERE,
+              String.format("Failed to load AI Image: %s", e));
+          return new Image(Objects.requireNonNull(
+              MainStage.class.getResourceAsStream("/Image/Window/Background.png")));
         }
       }
     };
 
-    // Update the UI once the image is loaded
     loadImageTask.setOnSucceeded(event -> {
-      if (USE_AI) {
-        Image roomImage = loadImageTask.getValue();
-        ImageView roomImageView = new ImageView(roomImage);
-        roomImageView.setFitWidth(1024);
-        roomImageView.setFitHeight(768);
-        content.getChildren().remove(0);
-        content.getChildren().add(0, roomImageView);
-      }
+      Image roomImage = loadImageTask.getValue();
+      ImageView roomImageView = new ImageView(roomImage);
+      roomImageView.setFitWidth(1024);
+      roomImageView.setFitHeight(768);
+      content.getChildren().remove(0);
+      content.getChildren().add(0, roomImageView);
     });
 
-    // Start the background task to load the image
     Thread loadImageThread = new Thread(loadImageTask);
     loadImageThread.setDaemon(true);
     loadImageThread.start();
-
-    return contentContainer;
   }
 }
