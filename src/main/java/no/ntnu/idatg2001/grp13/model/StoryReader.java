5cbec14257e8da29898164c097178a7e36e0528d
Arthur Borger Thorkildsen <arthurbt@ntnu.no>
2023-05-22 02:16:19 +0200

    Formatted StoryReader

--- src/main/java/no/ntnu/idatg2001/grp13/model/StoryReader.java
+++ src/main/java/no/ntnu/idatg2001/grp13/model/StoryReader.java
@@ -22,92 +22,92 @@ import no.ntnu.idatg2001.grp13.model.actions.ScoreAction;
 
 public class StoryReader {
 
-    private StoryReader(){
-        // empty, private constructor
+  private StoryReader() {
+    // empty, private constructor
+  }
+
+  /**
+   * The method reads a file and creates a story object from the file.
+   *
+   * @param fileName The name of the file to read from.
+   * @return A story object.
+   */
+  public static Story readFromFile(String fileName) {
+    String lineOfText;
+    Story story = null;
+    Passage passage = null;
+    Link link = null;
+    Path filePath = Path.of(fileName);
+
+    if (!Files.exists(filePath)) {
+      throw new IllegalArgumentException("File does not exist: " + fileName);
     }
 
-    /**
-     * The method reads a file and creates a story object from the file.
-     *
-     * @param fileName The name of the file to read from.
-     * @return A story object.
-     */
-    public static Story readFromFile(String fileName){
-        String lineOfText;
-        Story story = null;
-        Passage passage = null;
-        Link link = null;
-        Path filePath = Path.of(fileName);
+    try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+      String titleOfGame = reader.readLine();
 
-        if (!Files.exists(filePath)) {
-            throw new IllegalArgumentException("File does not exist: " + fileName);
+      if (titleOfGame == null || titleOfGame.isEmpty()) {
+        throw new IllegalArgumentException("File does not contain any text.");
+      }
+
+      while ((lineOfText = reader.readLine()) != null) {
+        if (lineOfText.startsWith("::")) {
+          String[] line = lineOfText.split("::");
+          passage = new Passage(line[1], reader.readLine());
+
+          if (story == null) {
+            story = new Story(titleOfGame, passage);
+          }
+          story.addPassage(passage);
         }
+        if (lineOfText.startsWith("[") && (passage != null)) {
+          String[] line = lineOfText.split("\\[");
+          String[] text = line[1].split("]");
 
-        try(BufferedReader reader = Files.newBufferedReader(filePath)) {
-            String titleOfGame = reader.readLine();
-
-            if (titleOfGame == null || titleOfGame.isEmpty()) {
-                throw new IllegalArgumentException("File does not contain any text.");
-            }
-
-            while ((lineOfText = reader.readLine()) != null){
-                if (lineOfText.startsWith("::")){
-                    String[] line = lineOfText.split("::");
-                    passage = new Passage(line[1], reader.readLine());
-
-                    if (story == null) {
-                        story = new Story(titleOfGame, passage);
-                    }
-                    story.addPassage(passage);
-                }
-                if (lineOfText.startsWith("[") && (passage != null )){
-                    String[] line = lineOfText.split("\\[");
-                    String[] text = line[1].split("]");
-
-                    line = text[1].split("\\(");
-                    String[] reference = line[1].split("\\)");
-                    link = new Link(text[0], reference[0]);
-                    passage.addLink(link);
-                }
-                if (lineOfText.startsWith("=") && link != null){
-                    // Adds an action to the link object.
-                    link.addAction(readActionFromFile(lineOfText));
-                }
-            }
+          line = text[1].split("\\(");
+          String[] reference = line[1].split("\\)");
+          link = new Link(text[0], reference[0]);
+          passage.addLink(link);
         }
-        catch (IOException e){
-            System.out.println(e);
+        if (lineOfText.startsWith("=") && link != null) {
+          // Adds an action to the link object.
+          link.addAction(readActionFromFile(lineOfText));
         }
-        return story;
+      }
+    } catch (IOException e) {
+      System.out.println(e);
     }
+    return story;
+  }
 
-    /**
-     * A method for reading action from a CSV file format.
-     * @param actionFromFile A string with the action to be read.
-     * @return the Action.
-     */
-    private static Action readActionFromFile(String actionFromFile){
-        String[] action = actionFromFile.split("=");
-        action = action[1].split(";");
+  /**
+   * A method for reading action from a CSV file format.
+   *
+   * @param actionFromFile A string with the action to be read.
+   * @return the Action.
+   */
+  private static Action readActionFromFile(String actionFromFile) {
+    String[] action = actionFromFile.split("=");
+    action = action[1].split(";");
 
-        switch (action[0]){
-            // Parsing it to integer since the input from the file is an int.
-            case ("Gold") -> {
-                return (new GoldAction(Integer.parseInt(action[1])));
-            }
-            case ("Health") -> {
-                return (new HealthAction(Integer.parseInt(action[1])));
-            }
-            case ("Score") -> {
-                return (new ScoreAction(Integer.parseInt(action[1])));
-            }
-            // Inventory action uses String as datatype.
-            case ("Inventory") -> {
-                return (new InventoryAction(action[1]));
-            }
+    switch (action[0]) {
+      // Parsing it to integer since the input from the file is an int.
+      case ("Gold") -> {
+        return (new GoldAction(Integer.parseInt(action[1])));
+      }
+      case ("Health") -> {
+        return (new HealthAction(Integer.parseInt(action[1])));
+      }
+      case ("Score") -> {
+        return (new ScoreAction(Integer.parseInt(action[1])));
+      }
+      // Inventory action uses String as datatype.
+      case ("Inventory") -> {
+        return (new InventoryAction(action[1]));
+      }
 
-            default -> System.err.print("error in switch");
-        }
-        return null;
+      default -> System.err.print("error in switch");
     }
+    return null;
+  }
 }
