f4855ea1bf70ad3303c3effc1621445a7dd0e13c
Torgrim Thorsen <torgrilt@stud.ntnu.no>
2023-05-23 15:09:47 +0200

    Removed irrelevant hash tests, removed test dependency

--- src/test/java/HashCodeContractTest.java
+++ src/test/java/HashCodeContractTest.java
@@ -1,26 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
 import no.ntnu.idatg2001.grp13.model.Link;
 import no.ntnu.idatg2001.grp13.model.Passage;
 import no.ntnu.idatg2001.grp13.model.Story;
 import org.junit.jupiter.api.Test;
 
 class HashCodeContractTest {
-  @Test
-  void equalsHashCodeContracts() {
-    EqualsVerifier.forClass(Link.class)
-        .withOnlyTheseFields("reference")
-        .verify();
-  }
-
-  @Test
-  void equalsAndHashCodeForPassageObjectTest() {
-    EqualsVerifier.forClass(Passage.class)
-            .withOnlyTheseFields("title")
-            .usingGetClass()
-            .verify();
-  }
 
   @Test
   void testEqualsForStoryObject() {
